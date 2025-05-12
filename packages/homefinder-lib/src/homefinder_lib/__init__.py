44dd326e9e0e74e817f83252df175798ae3dc4cb
Martins Zariņš <pasts@zarins.zip>
2025-05-12 17:58:45 +0000

    Made import paths relative

--- packages/homefinder-lib/src/homefinder_lib/__init__.py
+++ packages/homefinder-lib/src/homefinder_lib/__init__.py
@@ -5,11 +5,11 @@ This includes a class for homes, a protocol for finding them
 online, and so on.
 '''
 
-from home import (
+from .home import (
 	Home
 )
 
-from source import (
+from .source import (
 	Source,
 	SearchParams,
 	SearchIter
